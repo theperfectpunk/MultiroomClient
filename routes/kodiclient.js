ce706d77a87c82cb7c7d52f908d3d6544911694d
Mohit Tokas <mohittokas@live.com>
2019-03-28 20:03:48 +0530

    add seek test

--- routes/kodiclient.js
+++ routes/kodiclient.js
@@ -14,7 +14,31 @@ router.post('/play', function(req, res, next) {
     })
     .then((response) => {
       //debugger;
-      res.send({message: "success"});
+      setTimeout(() => {
+        fetch('http://127.0.0.1:8080/jsonrpc', {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            "jsonrpc": "2.0",
+            "method": "Player.Seek",
+            "params": {
+              "playerid": 0,
+              "value": {
+                "time": {
+                  "hours": 0,
+                  "milliseconds": 845,
+                  "minutes": 0,
+                  "seconds": 4
+                }
+              }
+            },
+            "id": 0
+          })
+        })
+        .then(() => {
+          res.send({message: "success"});
+        })
+      }, 2000)
     })
   }, req.body.timestamp - new Date().getTime())
 });
